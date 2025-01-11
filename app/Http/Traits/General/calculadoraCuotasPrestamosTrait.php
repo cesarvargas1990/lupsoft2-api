233f0750e38db98c8f0a5938565fcd130b07cfbd
cesar vargas <cesara.vargas1990@gmail.com>
2025-01-11 18:42:17 -0500

    ajustes logica y formulas prestamos

--- app/Http/Traits/General/calculadoraCuotasPrestamosTrait.php
+++ app/Http/Traits/General/calculadoraCuotasPrestamosTrait.php
@@ -11,118 +11,84 @@ use App\PsEmpresa;
 trait calculadoraCuotasPrestamosTrait
 {
 
-    public function calcularCuota($request)
-    {
+    public function calcularCuota($request){
 
         setlocale(LC_ALL,"es_ES@euro","es_ES","esp");
         $id_forma_pago = $request->get('id_forma_pago');
         $formaPago = Psformapago::find( $id_forma_pago);
         $id_periodo_pago = $formaPago->id_periodo_pago;
-        $sistemaPrestamo = $formaPago->codtipsistemap;
+        $sistemaPrestamo =$request->get('id_sistema_pago');
 
         if ($request->has('numcuotas') ){
             $numcuotas = $request->get('numcuotas');
         } else {
             $numcuotas = $formaPago->numcuotas;
         }
-
         if ($request->has('porcint') ){
             $porcint = $request->get('porcint'); 
         } else {
             $porcint = $formaPago->porcint;
         }
-        
-      
-
         if ($request->has('valorpres')) {
             $valorpres = $request->get('valorpres');
         } else {
             $valorpres = $formaPago->valorpres;
         }
-
         // Sistema frances
         if ($sistemaPrestamo == 1) {
-
             return $this->sistemaPrestMetodoFrances($request,$valorpres,$porcint,$id_periodo_pago,$numcuotas);
-
         // Sistema ingles
         } else if ($sistemaPrestamo == 2) {
-
             return $this->sistemaPrestMedodoIngles($request,$valorpres,$porcint,$id_periodo_pago,$numcuotas);
-
         } else {
+        // todo: Implementar logica sistema aleman u otro
             return [];
         }
-
-       
     }
 
-    function generarTablaAmortizacion ($request) {
+    function generarTablaAmortizacion($request) {
 
         $nit_empresa = $request->get('nitempresa');
-
         $id_forma_pago = $request->get('id_forma_pago');
-        $formaPago = Psformapago::find( $id_forma_pago);
-        $sistemaPrestamo = $formaPago->codtipsistemap;
-
-        $empresa = Psempresa::where('nitempresa',$nit_empresa )->first();
-
+        $formaPago = Psformapago::find($id_forma_pago);
+        $sistemaPrestamo = $request->get('id_sistema_pago');
+        $empresa = Psempresa::where('nitempresa', $nit_empresa)->first();
+    
+        // Cálculo de los valores de la tabla de amortización
         $datos = $this->calcularCuota($request);
-
-    
-        if ( array_key_exists('tabla',$datos) ) {
-
+        if (array_key_exists('tabla', $datos)) {
+            // Sistema Francés
             if ($sistemaPrestamo == 1) {
-
                 foreach ($datos['tabla'] as $dato) {
-
                     $tabla[] = array(
                         'N° Cuota' => $dato['indice'],
-                        'Fecha Cuota' =>  $dato['fecha_cuota_descrpcion'],
-                        'Interes' => number_format($dato['interes'],2),
-                        'Amortizacion' => number_format( $dato['amortizacion'],2),
-                        'Saldo' => number_format($dato['saldo'],2),
-                        'D. final' => number_format($dato['d_final'],2),
-                        'Cuota Fija Mensual' => '$'.' '.number_format($dato['cfija_mensual'],2),
-                        'Total a pagar por mes' =>   '$'.' '.number_format(round($dato['t_pagomes'],2),2)
-        
+                        'Fecha Cuota' => $dato['fecha_cuota_descrpcion'],
+                        'Interes' => '$' . ' ' . number_format($dato['interes'], 2),
+                        'Amortizacion' => '$' . ' ' . number_format($dato['amortizacion'], 2),
+                        'Saldo' => '$' . ' ' . number_format($dato['saldo'], 2),
+                        'Cuota Fija Mensual' => '$' . ' ' . number_format($dato['cfija_mensual'], 2),
+                        'Total a pagar por mes' => '$' . ' ' . number_format($dato['t_pagomes'], 2)
                     );
-        
                 }
-    
                 return $tabla;
-
             } else if ($sistemaPrestamo == 2) {
-
-                foreach ($datos['tabla'] as $dato) {
-
-                    $tabla[] = array(
-                        'N° Cuota' => $dato['indice'],
-                        'Fecha Cuota' =>  $dato['fecha_cuota_descrpcion'],
-                        'Interes' => number_format($dato['interes'],2),
-                        'Capital' => '$'.' '.number_format($dato['cfija_mensual'],2),
-                        'Total a pagar por mes' =>   '$'.' '.number_format(round($dato['t_pagomes'],2),2)
-        
-                    );
-        
-                }
-    
-                return $tabla; 
-
+               foreach ($datos['tabla'] as $dato) {
+				    $tabla[] = array(
+				        'N° Cuota' => $dato['indice'],
+				        'Fecha Cuota' => $dato['fecha_cuota_descrpcion'],
+				        'Interes' => '$' . ' ' . number_format($dato['interes'], 2), // Formatear interés
+				        'Capital' => '$' . ' ' . number_format($dato['amortizacion'], 2), // Amortización de capital
+				        'Saldo' => '$' . ' ' . number_format($dato['saldo'], 2), // Saldo restante
+				        'Total a pagar por mes' => '$' . ' ' . number_format($dato['t_pagomes'], 2) // Total de la cuota
+				    );
+				}
+				return $tabla;
             }
-
-            
-
         } else {
             return [];
         }
-        
-
-        
-
-
-
     }
+    
 
     function SpanishDate($FechaStamp)
     {
@@ -161,116 +127,108 @@ trait calculadoraCuotasPrestamosTrait
         }
     }
 
-    function sistemaPrestMetodoFrances($request,$valorpres,$porcint,$id_periodo_pago,$numcuotas) {
+    function sistemaPrestMetodoFrances($request, $valorpres, $porcint, $id_periodo_pago, $numcuotas) {
 
         $fec_inicial = $request->get('fec_inicial');
         $fecha = $fec_inicial;
         $date = new \DateTime($fecha);
+        // Configuraciones iniciales
         $fechas[1] = $date->format('Y-m-d');
+        $capital[0] = $valorpres;
         $salestax = 0;
         $dpayment = 0;
-        $capital[0] = $valorpres;
-        $valorCuota =  round(  round(($valorpres * ($salestax / 100 + 1) - $dpayment) * (($porcint / 100) / 12) / (1 - pow((1 + ($porcint / 100) / 12), (-$numcuotas))), PHP_ROUND_HALF_DOWN) , 0);
-        $tem = ($porcint / 100) / 12; // TASA EFECTIVA MENSUAL
-
+        // Tasa efectiva mensual
+        $tem = ($porcint / 100) / 12;
+        // Cálculo de la cuota fija mensual
+        $valorCuota = round(
+            ($valorpres * ($salestax / 100 + 1) - $dpayment) 
+            * ($tem) / (1 - pow((1 + $tem), (-$numcuotas))), 
+            2
+        );
         $tabla = [];
-       // $tabla['tabla']['ind_renovar'] = 0;
         for ($x = 1; $x <= $numcuotas; $x++) {
-            $fechas[$x] = $this->adicionarFechas($date,$id_periodo_pago);
-            $int = round($capital[$x - 1 ] * $tem);
-            $amort = $valorCuota - $int;
-            $capital[$x] = $capital[$x - 1 ] - $amort;
+            // Calcular la fecha de la cuota
+            $fechas[$x] = $this->adicionarFechas($date, $id_periodo_pago);
+    
+            // Calcular los valores de interés, amortización y saldo
+            $int = round($capital[$x - 1] * $tem, 2); // Interés de la cuota
+            $amort = round($valorCuota - $int, 2); // Amortización
+    
+            // Ajustar en la última cuota
+            if ($x == $numcuotas) {
+                // Forzar que la última cuota elimine cualquier saldo restante
+                $amort = round($capital[$x - 1], 2); // Amortización igual al saldo restante
+                $valorCuota = $amort + $int; // Ajustar la cuota para que coincida
+                $capital[$x] = 0; // Saldo final en $0
+            } else {
+                // Reducir el saldo normalmente
+                $capital[$x] = round($capital[$x - 1] - $amort, 2);
+            }
+    
+            // Calcular D. Final (saldo después del pago de la cuota)
+            $d_final = $capital[$x - 1] - $amort;
+    
+            // Agregar fila a la tabla
             $tabla['tabla'][] = array(
                 'indice' => $x,
-                'fecha_cuota_descrpcion' =>  $this->SpanishDate( strtotime($fechas[$x])),
+                'fecha_cuota_descrpcion' => $this->SpanishDate(strtotime($fechas[$x])),
                 'fecha' => $fechas[$x],
                 'interes' => $int,
                 'amortizacion' => $amort,
-                'saldo' => $capital[$x - 1 ],
-                'd_final' => $capital[$x - 1 ] -  $valorCuota,
-                'cfija_mensual' => $valorCuota,
-                't_pagomes' => $valorCuota,
-                'ind_renovar' => 0
+                'saldo' => $capital[$x], // Saldo restante después de la cuota
+              
+                'cfija_mensual' => $valorCuota, // Cuota fija mensual
+                't_pagomes' => $valorCuota      // Total a pagar (constante)
             );
         }
-
-        $tabla['datosprestamo'] = array(
-        'tem' => $tem,
-        'valor_cuota' => $valorCuota
-        );
-
-        return $tabla;
-    }
-
-    function sistemaPrestMedodoIngles ($request,$valorpres,$porcint,$id_periodo_pago,$numcuotas) {
-
-
-        $fec_inicial = $request->get('fec_inicial');
-        $fecha = $fec_inicial;
-        $date = new \DateTime($fecha);
-        $fechas[0] = $date->format('Y-m-d');
-        $salestax = 0;
-        $dpayment = 0;
-        $capital[0] = $valorpres;
-        $valorCuota = round($valorpres * ($porcint / 100));
-        $tem = ($porcint / 100) / 12; // TASA EFECTIVA MENSUAL
-        $tabla['tabla'][0]['cfija_mensual'] = 0;
-        $tabla['tabla'][0]['indice'] =  '-';
-        $tabla['tabla'][0]['fecha_cuota_descrpcion'] =   $this->SpanishDate( strtotime($fechas[0]));
-        $tabla['tabla'][0]['fecha'] =    $fechas[0];
-        $tabla['tabla'][0]['interes'] =     0;
-        $tabla['tabla'][0]['amortizacion'] = 0;
-        $tabla['tabla'][0]['saldo'] = 0;
-        $tabla['tabla'][0]['d_final'] = 0;
-        $tabla['tabla'][0]['cfija_mensual'] = 0;
-        $tabla['tabla'][0]['ind_renovar'] = 0;
-
-        for ($x = 1; $x < $numcuotas; $x++) {
-            //$x++;
-            $fechas[$x] = $this->adicionarFechas($date,$id_periodo_pago);
-           
-            $tabla['tabla'][] = array(
-                'indice' => $x,
-                'fecha_cuota_descrpcion' =>  $this->SpanishDate( strtotime($fechas[$x])),
-                'fecha' => $fechas[$x],
-                'interes' => $valorCuota,
-                'amortizacion' => 0,
-                'saldo' => 0,
-                'd_final' => 0,
-                'cfija_mensual' => 0,
-                't_pagomes' => $valorCuota,
-                'ind_renovar' => 0
-            );
-        }
-
-
-        $fechas[$x] = $this->adicionarFechas($date,$id_periodo_pago);
-        $tabla['tabla'][$x]['cfija_mensual'] = 0;
-        $tabla['tabla'][$x]['indice'] =  $x;
-        $tabla['tabla'][$x]['fecha'] =    $fechas[$x];
-        $tabla['tabla'][$x]['fecha_cuota_descrpcion'] =   $this->SpanishDate( strtotime($fechas[$x]));
-        
-        $tabla['tabla'][$x]['interes'] =     $valorCuota;
-        $tabla['tabla'][$x]['amortizacion'] = 0;
-        $tabla['tabla'][$x]['saldo'] = 0;
-        $tabla['tabla'][$x]['d_final'] = 0;
-        $tabla['tabla'][$x]['cfija_mensual'] = $valorpres;
-        $tabla['tabla'][$x]['t_pagomes'] = $valorpres + $valorCuota;
-        $tabla['tabla'][$x]['ind_renovar'] = 1;
-        // en esta ultima se agrega marca para hacer el nuevo ciclo completo
-
-
-
-
+        // Agregar detalles del préstamo
         $tabla['datosprestamo'] = array(
             'tem' => $tem,
             'valor_cuota' => $valorCuota
-            );
-    
-            return $tabla;
-
-
+        );
+        return $tabla;
     }
+    
+    function sistemaPrestMedodoIngles($request, $valorpres, $porcint, $id_periodo_pago, $numcuotas) {
+        $fec_inicial = $request->get('fec_inicial');
+        $fecha = $fec_inicial;
+        $date = new \DateTime($fecha);
+        // Configuraciones iniciales
+        $tem = ($porcint / 100) / 12; // Tasa efectiva mensual
+        $interesFijo = round($valorpres * $tem, 2); // Intereses fijos en cada cuota
+        $tabla = [];
 
+        for ($x = 1; $x <= $numcuotas; $x++) {
+            // Generar fechas de las cuotas
+            $fechas[$x] = $this->adicionarFechas($date, $id_periodo_pago);
+            if ($x < $numcuotas) {
+                // Cuotas intermedias: solo intereses
+                $tabla['tabla'][] = array(
+                    'indice' => $x,
+                    'fecha_cuota_descrpcion' => $this->SpanishDate(strtotime($fechas[$x])),
+                    'fecha' => $fechas[$x],
+                    'interes' => $interesFijo,
+                    'amortizacion' => 0, // No se amortiza el capital
+                    'saldo' => $valorpres, // El saldo permanece igual
+                    'cfija_mensual' => 0, // Sin amortización en las cuotas intermedias
+                    't_pagomes' => $interesFijo // Total a pagar por mes (solo intereses)
+                );
+            } else {
+                // Última cuota: intereses + capital
+                $tabla['tabla'][] = array(
+                    'indice' => $x,
+                    'fecha_cuota_descrpcion' => $this->SpanishDate(strtotime($fechas[$x])),
+                    'fecha' => $fechas[$x],
+                    'interes' => $interesFijo,
+                    'amortizacion' => $valorpres, // Amortización completa del capital
+                    'saldo' => 0, // El saldo se reduce a $0
+                    'cfija_mensual' => $valorpres, // Se paga el capital completo
+                    't_pagomes' => $interesFijo + $valorpres // Total: capital + intereses
+                );
+            }
+        }
+
+        return $tabla;
+    }
 
 }
