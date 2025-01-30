edfc3c66d64353574dcaf3a61fde25d3ba2eaaad
cesar vargas <cesara.vargas1990@gmail.com>
2025-01-29 19:31:33 -0500

    query tabla

---
+++ database/seeds/PsQuerytablaTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PsQuerytablaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('psquerytabla')->insert([
+            'id' => 1,
+            'codigo' => '1',
+            'sql' => "SELECT 
+                @rownum := @rownum + 1 AS numero_cuota,
+                id,
+                DATE_FORMAT(fecha_pago, '%d/%m/%Y') AS fecha_pago,
+                valor_pagar
+            FROM 
+                psfechaspago, 
+                (SELECT @rownum := 0) r
+            WHERE 
+                id_prestamo = {id_prestamo}
+            ORDER BY 
+                numero_cuota ASC",
+            'nitempresa' => '12345'
+          
+        ]);
+    }
+}
