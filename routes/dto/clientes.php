4336a73c6d5a14dff9d46b2697a2a1bf4836a980
Cesar Augusto Vargas <cesara.vargas1990@gmail.com>
2026-02-26 11:25:51 -0500

    fix: update request body structure for POST /psclientes to include optional fields for better data handling

--- routes/dto/clientes.php
+++ routes/dto/clientes.php
@@ -5,7 +5,7 @@ return [
     'GET /psclientes/{id}' => ['params' => ['id' => 'int'], 'body' => null, 'response' => ['object', 'object{message:string}']],
     'POST /psclientes' => [
         'params' => [],
-        'body' => 'object{nomcliente:string,id_tipo_docid:int,numdocumento:string,ciudad:string,celular:string,id_empresa:int,id_cobrador:int,id_user:int,email:string,fch_expdocumento:string,fch_nacimiento:string}',
+        'body' => 'object{nomcliente:string,id_tipo_docid:int,numdocumento:string,id_empresa:int,id_cobrador:int,id_user:int,ciudad?:string,telefijo?:string,celular?:string,direcasa?:string,diretrabajo?:string,ubicasa?:string,ubictrabajo?:string,ref1?:string,ref2?:string,email?:string,fch_expdocumento?:string,fch_nacimiento?:string}',
         'response' => 'object',
     ],
     'PUT /psclientes/{id}' => ['params' => ['id' => 'int'], 'body' => 'object', 'response' => 'object'],
